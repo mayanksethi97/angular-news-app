c32b82fb281b47cf90bfafbed45e5fb2987d1aab
Mayank Sethi <sethimayank97@gmail.com>
2021-01-03 20:39:13 +0530

    ignore

--- SmartNews/src/app/classes/root-config.ts
+++ SmartNews/src/app/classes/root-config.ts
@@ -2,7 +2,7 @@
 
 export const apiEndpoints = {
     geoApify: {
-        geoCoding: 'https://api.geoapify.com/v1/geocode/search?text=&apiKey=YOUR_API_KEY',
+        geoCoding: 'https://api.geoapify.com/v1/geocode/search',
         reverseGeoCoding: 'https://api.geoapify.com/v1/geocode/reverse',
         autoComplete: 'https://api.geoapify.com/v1/geocode/autocomplete'
     }
