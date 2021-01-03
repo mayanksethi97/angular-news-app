17127a4356f0a414927da617de89192f8ac24fff
Mayank Sethi <sethimayank97@gmail.com>
2021-01-03 20:45:50 +0530

    ignore

---
+++ SmartNews/src/app/classes/api-keys.ts
@@ -0,0 +1,5 @@
+export const API_KEYS = {
+    geoApify: '6f4d00b2f56b442d9b4542bed73a3d60',
+    openWeather: '33376b075f82c9fd56a1df6b2d5399ec',
+    mapbox: 'pk.eyJ1Ijoic2V0aGltYXlhbms5NyIsImEiOiJja2kwZGtvMXowb3drMnJwZTloenY2NmwyIn0.6MJthD64xjhdNk8u3Ah3lg'
+}
